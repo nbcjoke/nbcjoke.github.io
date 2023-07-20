0cb007e7f9526991a1f92a787918fe1e4925528d
Nikita Dymont <gg.joke55@gmail.com>
2023-07-20 18:48:01 +0300

    add information modal for products

---
+++ index.js
@@ -0,0 +1,7 @@
+const openInformation = () => {
+  document.querySelector(".mySidenav").style.width = "440px";
+};
+
+const closeInformation = () => {
+  document.querySelector(".mySidenav").style.width = "0";
+};
